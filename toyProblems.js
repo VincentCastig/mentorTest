9ca3a36f906f72f7e987656e7d6270b7cc30a158
VincentCastig <vincent.castig@gmail.com>
2017-09-11 11:59:02 -0500

    Completed

--- toyProblems.js
+++ toyProblems.js
@@ -1,27 +1,77 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
+  reverseIt: (str) => {
+      str = str.split('').reverse().join('');
+  },
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
 
-  // removeDups:
+  removeDups: (Arr) => {
+      let x = [];
+      
+      for (i=0; i < arr.length; i++){
+        if(!x.includes(arr[i])) {
+          x.push(arr[i])
+        }
+      }
+      return x;
+    
+  },
 
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
 
-  // titleIt:
+  titleIt: (str) => {
+    str = str.split(' ') 
+    const x = [];
+    for (word of str) {
+      for (i=0; i<word.length; i++){
+        if(i === 0){
+          x.push(word[i].toUpperCase())
+        }
+        else{
+          x.push(word[i])
+        }
+      }
+      x.push(" ")
+    }
+    return x.join('');
+  },
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
   //capitalize the first letter of each word
 
-  // vowelCounter:
+  vowelCounter: (str) => {
+    var val = "aeiouAEIOU";
+    var x = 0;
+    
+    for (i = 0; i < str.length; i++){
+        if(str[i].match(/[aeiou]/ig)) {
+          console.log(x)
+          x++;
+        }
+    }
+    return x
+   },
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
 
-  // isPrime:
+  isPrime: (num) => {
+    let counter = 0;
+    
+    for (i=1; i < num; i++){
+      if(num % i === 0) {
+        counter++;
+      }
+    }
+    if(counter > 1) {
+      return false;
+    }
+    return true;
+  },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
@@ -30,7 +80,7 @@ module.exports = {
   //var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  //foo:
+  foo: '1020',
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
@@ -41,6 +91,6 @@ module.exports = {
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
 
-  // log1:
-  // log2:
-}
+  log1: "Hello World",
+  log2: "Hello undefined"
+};
